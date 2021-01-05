f44e385dfcde3671ebe7cb7f41e5b50db7f9e01f
Jakob Neufeld <jakob.n.neufeld@gamil.com>
2021-01-05 20:09:53 +0100

    Updated metadata

--- spec.rs
+++ spec.rs
@@ -4,6 +4,8 @@ fn main() {
     .author("Jakob Neufeld")
     .name("lorry-cli")
     .version("0.1.4")
+    .readme("Readme.md")
+    .description("A cli for lorry")
     .dependencie("indicatif", "0.15.0")
     .gen();
 }
